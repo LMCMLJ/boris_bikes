f62e7dc8bb2b707b4fe433d71f35819f3dc819ee
James Gardiner <jamesgardiner075@gmail.com>
2016-09-28 18:03:00 +0100

    Refactor release_bike to use guard clause

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,11 +4,8 @@ class DockingStation
   attr_accessor :bike
 
   def release_bike
-    if @bike == nil
-      raise "No bike available"
-    else
-   Bike.new
-    end
+    raise "No bike available" if @bike == nil
+    Bike.new
   end
 
   def dock(bike)
